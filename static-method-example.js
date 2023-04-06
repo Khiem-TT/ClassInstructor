3f34c0495fc47da12c2054aef73f58bc3b6bf95e
tranthekhiem14112001 <trankhiem99999@gmail.com>
2023-04-06 10:50:49 +0700

    first commit

---
+++ static-method-example.js
@@ -0,0 +1,29 @@
+class Instructor {
+    constructor(name, role = "assistant") {
+        this.role = role;
+        this.name = name;
+    }
+
+    renderDetails() {
+        console.log(`${this.name} - ${this.role}`);
+    }
+
+    static helloWorld() {
+        console.log('Hi there');
+    }
+
+    static canTeach(instructor) {
+        return instructor.role === 'classroom';
+    }
+}
+
+let juniorInstructor = new Instructor('Brian');
+let seniorInstructor = new Instructor('Alice', "classroom");
+
+juniorInstructor.renderDetails();
+seniorInstructor.renderDetails();
+
+Instructor.helloWorld();
+
+console.log(`${juniorInstructor.name} can teach: ${Instructor.canTeach(juniorInstructor)}`);
+console.log(`${seniorInstructor.name} can teach: ${Instructor.canTeach(seniorInstructor)}`);
